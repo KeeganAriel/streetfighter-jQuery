733042805a9054db73edab25eac644c7059ead4d
KeeganAriel <keegan.teinowitz@gmail.com>
2016-01-12 16:26:10 -0800

    JS:event rye throw show on mouse down

--- js/app.js
+++ js/app.js
@@ -8,8 +8,10 @@ $(document).ready(function() {
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
-		console.log('mousedown');
 		// play hadouken sound
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
 		// show hadouken and animate it to the right of the screen	
 	})
 	.mouseup(function() {
