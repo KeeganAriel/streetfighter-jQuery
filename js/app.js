92999a031c9893a566db316008ef889420cc70c3
KeeganAriel <keegan.teinowitz@gmail.com>
2016-01-14 10:12:02 -0800

    css: text format

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 	})
+
 	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
@@ -11,8 +12,7 @@ $(document).ready(function() {
 		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').finish().show()
-			.animate(
+		$('.hadouken').finish().show().animate(
   			{'left': '1020px'},
   			850,
   			function() {
@@ -25,12 +25,28 @@ $(document).ready(function() {
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
-		// ryu back to ready position
 	});
-});
 
-function playHadouken() {
-	$('#hadouken-sound')[0].volume = 0.5;
-	$('#hadouken-sound')[0].load();
-	$('#hadouken-sound')[0].play();
+	function playHadouken() {
+		$('#hadouken-sound')[0].volume = 0.5;
+		$('#hadouken-sound')[0].load();
+		$('#hadouken-sound')[0].play();
 }
+
+	$(document).keydown(function(event) {
+		if (event.which == 88) {
+			$('ryu-still').hide();
+			$('ryu-ready').hide();
+			$('ryu-cool').show();
+			console.log('test');
+			}
+		})
+
+	.keyup(function(event) {
+		if (event.which == 88) {
+			$('.ryu-still').show();
+			$('.ryu-cool').hide();
+			console.log('testing');
+			}
+		});
+});
